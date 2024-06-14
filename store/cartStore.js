5343fa52994d7ce23e27a06b2846ad22c1241595
Aryel Anne Louise de Souza Gabriel <aannelouise@outlook.com>
2024-06-14 13:11:21 -0300

    :sparkles: Cadastrar pedido

--- store/cartStore.js
+++ store/cartStore.js
@@ -1,9 +1,17 @@
 import { create } from "zustand";
 
+function removeItemOnce(arr, value) {
+    const index = arr.indexOf(value);
+    if (index > -1) {
+        arr.splice(index, 1);
+    }
+    return arr;
+}
+
 export const useCartStore = create((set) => {
     return {
         cart: [],
         addToCart: (item) => set((state) => ({cart: [...state.cart, item]})),
-        removeFromCart: (item) => set((state) => ({cart: state.cart.filter((sorvete) => sorvete?._id !== item?._id)}))
+        removeFromCart: (item) => set((state) => ({cart: removeItemOnce([...state.cart], item)}))
     }
 })
